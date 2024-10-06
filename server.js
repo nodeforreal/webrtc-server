940ea81707ed378a8d9ecd374d54e3426475e06e
KY <kaliyamoorthy.2k@gmail.com>
2024-10-06 10:21:51 +0530

    experiment: 1:n broadcast

--- server.js
+++ server.js
@@ -1,81 +1,56 @@
 const express = require("express");
-const path = require("path");
-const Websocket = require("ws");
 const wrtc = require("wrtc");
+const path = require("path");
 const app = express();
+
+let senderStream;
+
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.get("/broadcast", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "/public/broadcast.html"));
+});
+
+app.get("/consumer", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "/public/consumer.html"));
+});
+
+app.post("/broadcast", async (req, res) => {
+  const { sdp } = req.body;
+  const peer = new wrtc.RTCPeerConnection();
+  peer.ontrack = (e) => handleTrackEvent(e, peer);
+  const desc = new wrtc.RTCSessionDescription(sdp);
+  await peer.setRemoteDescription(desc);
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  res.json({
+    sdp: peer.localDescription,
+  });
+});
+
+app.post("/consumer", async (req, res) => {
+  const { sdp } = req.body;
+  const peer = new wrtc.RTCPeerConnection();
+  const desc = new wrtc.RTCSessionDescription(sdp);
+  await peer.setRemoteDescription(desc);
+  senderStream
+    .getTracks()
+    .forEach((track) => peer.addTrack(track, senderStream));
+  const ans = await peer.createAnswer();
+  await peer.setLocalDescription(ans);
+  res.json({
+    sdp: peer.localDescription,
+  });
+});
+
+function handleTrackEvent(ev, peer) {
+  console.log(ev.streams[0]);
+  senderStream = ev.streams[0];
+}
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.html"));
-});
-
 app.listen(PORT, () => {
-  console.log("Port is listening on " + PORT);
-});
-
-const websocket = new Websocket.Server({ port: 3300 });
-
-websocket.on("connection", (ws) => {
-  console.log("ws connection opened.");
-
-  const peer = new wrtc.RTCPeerConnection();
-  const dc = peer.createDataChannel("chat");
-
-  dc.onopen = () => {
-    console.log("webrtc datachannel connection established.");
-  };
-
-  dc.onmessage = (event) => {
-    console.log("on datachannel message:", event.data);
-  };
-
-  peer.onicecandidate = (e) => {
-    if (e.candidate) {
-      ws.send(
-        JSON.stringify({
-          event: "candidate",
-          data: e.candidate,
-        })
-      );
-      console.log("sent icecandidate to client.", e.candidate);
-    }
-  };
-
-  peer.ontrack = (event) => {
-    console.log("streams from client:", event);
-  };
-
-  peer
-    .createOffer()
-    .then((offer) => {
-      return peer.setLocalDescription(offer);
-    })
-    .then(() => {
-      ws.send(
-        JSON.stringify({
-          event: "offer",
-          data: peer.localDescription,
-        })
-      );
-      console.log("sent offer to the client.");
-    });
-
-  ws.on("message", (buffer) => {
-    const { event, data } = JSON.parse(buffer.toString());
-
-    if (event === "answer") {
-      console.log("answer received from client.", data);
-      peer.setRemoteDescription(data);
-    }
-
-    if (event === "candidate") {
-      console.log("candidate received from client.", data);
-      peer.addIceCandidate(data);
-    }
-  });
-
-  ws.on("close", () => {
-    console.log("ws disconnected.");
-  });
+  console.log(`Server has started and is listening on port: ${PORT}`);
 });
